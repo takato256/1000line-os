cf897c81200bae5d77fc08f2428cc56409384645
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-01-04 14:05:23 -0500

    [add] success first comple

--- run.sh
+++ run.sh
@@ -3,4 +3,12 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+CC=/usr/bin/clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+	kernel.c
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+	-kernel kernel.elf
